3dc5f465ded07bf61e8f28d3604b31e22baa61d1
Pawanyy <pawanyyy01@gmail.com>
2022-09-09 09:49:23 +0530

    added csrf token

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -3,23 +3,21 @@
         <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 rounded-xl p-6">
             <h1 class="text-center font-bold text-xl">Register</h1>
             <form action="/register" method="post" class="mt-10">
+
+                @csrf
+
                 <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="name">Name</label>
-                    <input class="border border-gray-400 p-2 w-full" 
-                        type="text" name="name" id="name" required>
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">Name</label>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name" required>
                 </div>
                 <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="username">Username</label>
-                    <input class="border border-gray-400 p-2 w-full" 
-                        type="text" name="username" id="username" required>
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">Username</label>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username" required>
                 </div>
                 <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="password">Password</label>
-                    <input class="border border-gray-400 p-2 w-full" 
-                        type="password" name="password" id="password" required>
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">Password</label>
+                    <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password"
+                        required>
                 </div>
                 <div class="mb-6">
                     <button type="submit"
