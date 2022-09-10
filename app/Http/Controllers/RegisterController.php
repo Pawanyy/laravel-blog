f3e368d488ef24a765edb529f7f933ae846f5002
Pawanyy <pawanyyy01@gmail.com>
2022-09-10 08:56:55 +0530

    added morevalidation in register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,10 +15,10 @@ class RegisterController extends Controller
     public function store()
     {
         $attributes = request()->validate([
-            'name' => ['required', 'min:3', 'max:255'],
-            'username' => ['required', 'min:3', 'max:255'],
-            'email' => ['required', 'email', 'min:3', 'max:255'],
-            'password' => ['required'], 'min:3', 'max:255',
+            'name'     => 'required|min:3|max:255',
+            'username' => 'required|min:3|max:255|unique:users,username',
+            'email'    => 'required|email|min:3|max:255|unique:users,email',
+            'password' => 'required|min:3|max:255',
         ]);
 
         User::create($attributes);
